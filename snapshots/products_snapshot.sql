f879f9e2adbc275ab41b57f851211d42237c226d
thanupriyaprasad@gmail.com <thanupriyaprasad@gmail.com>
2025-07-18 06:39:27 +0000

    ffj

--- snapshots/products_snapshot.sql
+++ snapshots/products_snapshot.sql
@@ -1,17 +1,17 @@
 {% snapshot products_snapshot %}
-  {{
+{{
     config(
-      target_schema='snapshots',  -- or your preferred schema for snapshots
-      unique_key='product_id',
-      strategy='check',
-      check_cols=['product_name', 'product_category', 'product_cost', 'product_price']
+        target_schema='snapshots',
+        unique_key='product_id',
+        strategy='check',
+        check_cols=['product_name', 'product_category', 'product_cost', 'product_price']
     )
-  }}
-  SELECT
+}}
+SELECT
     product_id,
     product_name,
     product_category,
     product_cost,
     product_price
-  FROM {{ ref('bronze_products') }}  -- or use the actual reference if you load raw tables as models/views
+FROM {{ source('MAVENDB', 'PRODUCTS') }}
 {% endsnapshot %}
