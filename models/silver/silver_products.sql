fdbe121d927ebb90c164fe50823007da14575025
thanupriyaprasad@gmail.com <thanupriyaprasad@gmail.com>
2025-07-18 11:35:51 +0000

    profit

--- models/silver/silver_products.sql
+++ models/silver/silver_products.sql
@@ -5,6 +5,18 @@ with deduped as (
         *,
         row_number() over (partition by product_id order by dbt_valid_from desc) as rn
     from {{ ref('products_snapshot') }}
+),
+
+cleaned as (
+    select
+        product_id,
+        product_name,
+        product_category,
+        -- Remove dollar sign, then cast to decimal
+        cast(replace(product_cost, '$', '') as decimal(10,2)) as product_cost,
+        cast(replace(product_price, '$', '') as decimal(10,2)) as product_price
+    from deduped
+    where rn = 1
 )
 
 select
@@ -14,5 +26,4 @@ select
     product_cost,
     product_price,
     product_price - product_cost as product_profit
-from deduped
-where rn = 1
+from cleaned
