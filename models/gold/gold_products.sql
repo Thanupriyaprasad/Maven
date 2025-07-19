dedc108f6a3e87f3c9da123548253062eab6c88f
thanupriyaprasad@gmail.com <thanupriyaprasad@gmail.com>
2025-07-19 17:41:02 +0000

    gold

--- models/gold/gold_products.sql
+++ models/gold/gold_products.sql
@@ -5,26 +5,15 @@ with products as (
         product_category,
         product_price,
         product_cost,
-        product_profit  
+        (product_price - product_cost) as product_profit
     from {{ ref('silver_products') }}
 ),
-
 
 sales_summary as (
     select
         product_id,
-        sum(units) as total_units_sold,
-        sum(units * sale_price) as total_sales_revenue
+        sum(units) as total_units_sold
     from {{ ref('gold_sales') }}
-    group by product_id
-),
-
-
-inventory_summary as (
-    select
-        product_id,
-        sum(stock_on_hand) as total_stock_on_hand
-    from {{ ref('gold_inventory') }}
     group by product_id
 )
 
@@ -32,4 +21,11 @@ select
     p.product_id,
     p.product_name,
     p.product_category,
-    p.product
+    p.product_price,
+    p.product_cost,
+    p.product_profit,
+    coalesce(s.total_units_sold, 0) as total_units_sold,
+    coalesce(s.total_units_sold, 0) * p.product_price as total_sales_revenue
+from products p
+left join sales_summary s on p.product_id = s.product_id
+order by total_units_sold desc
