e054c75b23d18b150d8171d182c43ca00c38b32b
thanupriyaprasad@gmail.com <thanupriyaprasad@gmail.com>
2025-07-19 17:33:50 +0000

    goldcal

--- models/gold/gold_calendar.sql
+++ models/gold/gold_calendar.sql
@@ -1,8 +1,7 @@
 with base as (
     select
-        cast(date as date) as date_actual
-    from {{ ref('silver_calendar') }}
-    
+        cast(calendar_date as date) as date_actual
+    from MAVENDB.PUBLIC.silver_calendar
 ),
 
 enriched as (
@@ -13,14 +12,12 @@ enriched as (
         extract(day from date_actual) as day,
         extract(quarter from date_actual) as quarter,
         extract(week from date_actual) as week_of_year,
-        extract(dow from date_actual) + 1 as day_of_week,  
+        extract(dow from date_actual) + 1 as day_of_week,
         to_char(date_actual, 'Day') as day_name,
         to_char(date_actual, 'Month') as month_name,
-        
         case when extract(dow from date_actual) in (5, 6) then 1 else 0 end as is_weekend,
         case when date_actual = date_trunc('month', date_actual) then 1 else 0 end as is_month_start,
         case when date_actual = last_day(date_actual, 'month') then 1 else 0 end as is_month_end
-        
     from base
 )
 
